7450b381b52106d67f065e6e10ae5e8ff5f6c17b
Shreyas M Iliger <109969895+lightningbolt0827@users.noreply.github.com>
2024-12-17 23:09:52 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,9 +27,9 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
+  // Instantiate the module under test (MUT):
+  tt_um_lightFP8 user_project (
+      
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
