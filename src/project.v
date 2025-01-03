50cfb51f5cadce66ff450441c3b8a61b84a8eb19
Shreyas M Iliger <109969895+lightningbolt0827@users.noreply.github.com>
2025-01-03 20:30:14 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,25 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Couo_outyright (c) 2024 Your Nui_inme
  * Suo_outDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
-
-module tt_um_lightFP8 (
+module tt_um_lightFP8 #(
 parameter SIGN_BITS = 1,           // Sign bit count (always 1 for IEEE 754)
 parameter EXP_BITS = 4,           // Number of exponent bits
 parameter MANTISSA_BITS = 3,     // Number of mantissa bits
 parameter BIAS = (1 << (EXP_BITS - 1)) - 1 // Bias for the exponent
 )(
+        input wire [7:0] ui_in, // Input operand ui_in
+        output wire [7:0] uo_out // Product
+        input wire [7:0] uio_in, // Input operand uio_in
+        output wire [7:0] uio_out,  // IOs: Output path
+        output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+        input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+        input  wire       clk,      // clock
+        input  wire       rst_n     // reset_n - low to reset
         
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
 );
-
 // All output pins must be assigned. If not used, assign to 0.
 //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
 assign uio_out = 0;
@@ -30,6 +28,7 @@ assign uio_oe  = 0;
 // List all unused inputs to prevent warnings
 wire _unused = &{ena, clk, rst_n, 1'b0};
     
+        
 // Internal signals
 wire [MANTISSA_BITS-1:0] Mout;
 wire [EXP_BITS-1:0] Eout;
@@ -50,12 +49,14 @@ wire [MANTISSA_BITS:0] M1bout = Mb[MANTISSA_BITS-1] ? {2'b11, Mb[MANTISSA_BITS-1
 wire [MANTISSA_BITS:0] M1addout = M1aout + M1bout;
 
 wire N1, N2, N3, Ce;
-nand (N1, Ma[MANTISSA_BITS-1], Mb[MANTISSA_BITS-1]);
-nor  (N2, Ma[MANTISSA_BITS-1], Mb[MANTISSA_BITS-1]);
-or   (N3, N2, M1addout[MANTISSA_BITS]);
-nand (Ce, N1, N3);
 
-assign Eout = Ea + Eb - BIAS - Ce;
+assign N1 = ~(Ma[MANTISSA_BITS-1] & Mb[MANTISSA_BITS-1]);
+assign N2 = ~(Ma[MANTISSA_BITS-1] | Mb[MANTISSA_BITS-1]);
+assign N3 = ~((~N2) & (~M1addout[MANTISSA_BITS]));
+assign Ce = ~(N1 & N3);
+
+assign Eout = Ea + Eb - BIAS +Ce;
 assign Mout = M1addout[MANTISSA_BITS] ? {M1addout[MANTISSA_BITS-2:0], 1'b0} : M1addout[MANTISSA_BITS-1:0];
 assign uo_out = {Sout, Eout, Mout};
+
 endmodule
