5b52b90cfa66b81e0c78554551c85eaf72985486
Shreyas M Iliger <109969895+lightningbolt0827@users.noreply.github.com>
2025-01-08 23:16:39 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,8 +1,8 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
+/* This testbench instantiates the module and provides wires 
+   for interaction, driven/tested by the cocotb test.py script.
 */
 module tb ();
 
@@ -25,15 +25,18 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
+  // Power and ground connections for gate-level testing
+  `ifdef GL_TEST
+  supply1 VPWR; // Define VPWR as a logic '1'
+  supply0 VGND; // Define VGND as a logic '0'
+  `endif
+
+  // Instantiate the module under test (Replace tt_um_lightFP8 with your module name)
   tt_um_lightFP8 user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
-
+      `ifdef GL_TEST
+      .VPWR(VPWR), // Power
+      .VGND(VGND), // Ground
+      `endif
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
@@ -44,15 +47,17 @@ module tb ();
       .rst_n  (rst_n)     // not reset
   );
 
-  // Assign values to the wire type inout signals
+  // Assign values to the wire-type inout signals
   assign ui_in = ui_in_reg;
   assign uio_in = uio_in_reg;
 
+  // Clock generation
   initial begin
     clk = 0;
     forever #5 clk = ~clk;  
   end
 
+  // Stimulus generation
   initial begin
     rst_n = 0;  
     ui_in_reg = 8'b00000000;
@@ -71,8 +76,8 @@ module tb ();
     ui_in_reg = 8'b00000111; // 7
     uio_in_reg = 8'b00000010; // 2
     #20;
-    ui_in_reg = 8'b00000010; // 0
-    uio_in_reg = 8'b00010000; // 0
+    ui_in_reg = 8'b00000010; // 2
+    uio_in_reg = 8'b00010000; // 16
     #20;
     ui_in_reg = 8'b00000001; // 1
     uio_in_reg = 8'b00000001; // 1
@@ -81,6 +86,7 @@ module tb ();
     $stop;
   end
 
+  // Monitor the signals during simulation
   initial begin
     $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b", $time, ui_in, uio_in, uo_out);
   end
