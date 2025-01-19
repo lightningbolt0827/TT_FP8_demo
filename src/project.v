a255733c583b86eb429c3a27ac3fdd2254d8a501
Shreyas M Iliger <109969895+lightningbolt0827@users.noreply.github.com>
2025-01-19 20:14:38 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -32,7 +32,6 @@ module tt_um_logarithmic_afpm (
     assign uio_oe  = 8'b0;
 
     // Prevent warnings for unused inputs
-    wire _unused = &{ena, 1'b0}; 
 
     // Declare wires for extracted fields
     wire [9:0] Ma, Mb;
