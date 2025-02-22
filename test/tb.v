0f1ab0055d413ebb0473d8581cc3f36ac7ec4148
Shreyas M Iliger <109969895+lightningbolt0827@users.noreply.github.com>
2025-02-22 15:11:45 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -100,7 +100,7 @@ module tb ();
 
   // Monitor the signals during simulation
 initial begin
-   $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b | reset=%b", $time, ui_in, uio_in, uo_out, rst_n);
+   $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b | reset=%b | clk=%b , $time, ui_in, uio_in, uo_out, rst_n);
    /*#10 $display("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b | rest=%b", $time, ui_in, uio_in, uo_out, rst_n);
     #10 $display("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b | rest=%b", $time, ui_in, uio_in, uo_out, rst_n);
     #10 $display("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b | rest=%b", $time, ui_in, uio_in, uo_out, rst_n);
