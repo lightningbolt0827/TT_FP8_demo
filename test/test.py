f46e7456ecbd979cb45b1c7dbb9ce4d6f0abc3ad
Shreyas M Iliger <109969895+lightningbolt0827@users.noreply.github.com>
2025-02-23 10:12:16 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,23 +8,24 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def test_project(dut):
+    
     dut._log.info("Start")
-
     # Manually set clk to 0 before starting the clock
     dut.clk.value = 0  # Ensure initial state is LOW
-    await Timer(10, units="ns")
-    # Set the clock period to 10 ns (100 MHz)
-    clock = Clock(dut.clk, 20, units="ns")
-    cocotb.start_soon(clock.start())
-
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 1)
-    #await Timer(5, units="ns")
+    await Timer(10, units="ns")
+    # Set the clock period to 10 ns (100 MHz)
+    clock = Clock(dut.clk, 20, units="ns")
+    cocotb.start_soon(clock.start())
+
+
+    #await ClockCycles(dut.clk, 1)
+    await Timer(10, units="ns")
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
@@ -34,13 +35,13 @@ async def test_project(dut):
     dut.uio_in.value = 0x00
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-    #await Timer(20, units="ns")
+    #await ClockCycles(dut.clk, 1)
+    await Timer(20, units="ns")
     dut.ui_in.value = 0x3E
     dut.uio_in.value = 0x42
 
-    await ClockCycles(dut.clk, 11)
-    #await Timer(205, units="ns")
+    #await ClockCycles(dut.clk, 11)
+    await Timer(205, units="ns")
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 0xDA
